17074215d20049c91115e2f8aaad7d19dec4dce2
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-12-19 22:58:15 +0100

    Added new role

--- src/main/java/com/jarkos/bss/persistance/enums/Role.java
+++ src/main/java/com/jarkos/bss/persistance/enums/Role.java
@@ -8,7 +8,8 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
 
     ROLE_ADMIN,
-    ROLE_USER;
+    ROLE_USER,
+    ROLE_CUSTOMER;
 
     //This GrantedAuthority interface implementation is to support SpringSecurity's role management cleanly
     @Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,8 @@
 INSERT INTO webapp.user (version,email,enabled,locked,password,username) VALUES (0, 'admin@test.com', 1, 0, '123456', 'admin');
 INSERT INTO webapp.user (version,email,enabled,locked,password,username) VALUES (0, 'jarkos@test.com', 1, 0, 'jarkos', 'jarkos');
 INSERT INTO webapp.user_role (role,user_id) VALUES ( '0', '1');
+INSERT INTO webapp.user_role (role,user_id) VALUES ( '1', '1');
 INSERT INTO webapp.user_role (role,user_id) VALUES ( '2', '1');
 INSERT INTO webapp.user_role (role,user_id) VALUES ( '0', '2');
+INSERT INTO webapp.user_role (role,user_id) VALUES ( '1', '2');
 INSERT INTO webapp.user_role (role,user_id) VALUES ( '2', '2');
